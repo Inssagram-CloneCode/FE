2f7136395164eef6c7059eeec55a986f20b10321
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-08-22 22:15:57 +0900

    Style: #5 Outline of style in the  main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import MainCard from '../components/MainCard'
-
+import Layout from '../components/layout/Layout'
+import Card from "react-bootstrap/Card";
 const MainPage = () => {
   return (
-    <div>
+    <Layout>
+    <Card>
+    <div className='layout'>
       <MainCard/>
-
+      <div className='miniCard'>
+       
+      </div>
     </div>
-
+    </Card>
+   </Layout>
   )
 }
 
