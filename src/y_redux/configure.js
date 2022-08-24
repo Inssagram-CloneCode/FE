da86421ea6470bc64afc32ee5748648fb48f539d
youngdong82 <nfs82young@gmail.com>
2022-08-25 07:48:36 +0900

    feat: #10 Create redux about modal
    모달과 관련된 redux 만들기

--- src/y_redux/configure.js
+++ src/y_redux/configure.js
@@ -1,7 +1,8 @@
 import { combineReducers, configureStore, } from '@reduxjs/toolkit';
 import userReducer from './modules/userSlice';
+import modalReducer from './modules/modalSlice';
 
-const rootReducer = combineReducers({user: userReducer});
+const rootReducer = combineReducers({user: userReducer, modal: modalReducer});
 
 const store = configureStore({reducer: rootReducer});
 // const store = configureStore({rootReducer, middleware: [...getDefaultMiddleware()]});
