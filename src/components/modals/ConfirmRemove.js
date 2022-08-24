a9c53e0817909b831b3d3de7abdd991b3e61d32f
youngdong82 <nfs82young@gmail.com>
2022-08-25 07:53:14 +0900

    feat: #10 Create PostWirte Basic Logic(NOT YET)

--- src/components/modals/ConfirmRemove.js
+++ src/components/modals/ConfirmRemove.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { clearAlert, clearAll } from '../../y_redux/modules/modalSlice';
 import '../../y_css/confirmRemove.css';
 
 
 const ConfirmRemove = () => {
+  const dispatch = useDispatch();
+  const removeAll = () => {
+    dispatch(clearAll());
+  }
+  const goBack = (e) => {
+    const {target:{className}} = e;
+    if(className === 'modal_background' || className === "confirm_btn_cancel"){
+      dispatch(clearAlert());
+    }
+  };
+
   return(
-    <div className="modal_board3">
-      <div className="confirm_remove_note">
-        <div className="confirm_remove_note_top">게시물을 삭제하시겠어요?</div>
-        <div className="confirm_remove_note_bottom">지금 나가면 수정 내용이 저장되지 않습니다.</div>
+    <div className="modal_background" onClick={goBack}>
+      <div className="modal_board3">
+        <div className="confirm_remove_note">
+          <div className="confirm_remove_note_top">게시물을 삭제하시겠어요?</div>
+          <div className="confirm_remove_note_bottom">지금 나가면 수정 내용이 저장되지 않습니다.</div>
+        </div>
+        <button className="confirm_btn_remove" onClick={removeAll}>삭제</button>
+        <button className="confirm_btn_cancel" onClick={goBack}>취소</button>
       </div>
-      <button className="confirm_btn_remove">삭제</button>
-      <button className="confirm_btn_cancel">취소</button>
     </div>
   )
 }
